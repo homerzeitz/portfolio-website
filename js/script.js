3e0174ea84a80399779c42f7372f957371470bea
Stephanie <mail@stephaniehotz.de>
2019-04-11 17:39:47 +0200

    add slide toggle in #welcome / startscreen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("BOOOM, welcome to the site of a conceptual bubble gum geek!");
-var text1 = "Hi";
-var text2 = "Hello, spaces are also OK!";
-var text3 = 'Single quotes are valid too.';
+
+$(function() {
+  $("#welcome").slideToggle(6000);
+  });

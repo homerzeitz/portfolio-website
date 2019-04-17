8174a9e9c6d4557fcc129eb59a5a3e7759139ff6
Stephanie <mail@stephaniehotz.de>
2019-04-17 12:56:40 +0200

    Add top button

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// When the user scrolls down 60px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
